cfd34dde86508cea682b505d347bec51cbee51c6
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-24 01:44:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  counter = seconds
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+  end  
+  
+ phrase=  "Happy New Year!"
+ return phrase.upcase
+end
+
+def countdown_with_sleep(seconds)
+  counter = seconds
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+    sleep(1)
+  end  
+  
+ phrase=  "Happy New Year!"
+ return phrase.upcase
 end
